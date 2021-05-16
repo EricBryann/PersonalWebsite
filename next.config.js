ae87661f3010ced3e95b5886a977ca333b312b5e
Eric Bryan <eric.bryan2001@gmail.com>
2021-05-16 16:54:23 +0800

    Commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     env: {
         MONGO_URI: "mongodb+srv://Eric:Eric@cluster0.94ex4.mongodb.net/postDB?retryWrites=true&w=majority",
-        BACKEND_URL: "http://localhost:3000/api/posts"
+        BACKEND_URL: "http://localhost:3000/api/posts",
+        CI:false
     },
 }
